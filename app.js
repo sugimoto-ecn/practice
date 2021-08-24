2152be487bff19fbf71dd0d7c636d4afc90c34f0
sugimo-ne <kein.sugimoto@gmail.com>
2021-08-25 01:18:15 +0900

    nodemon install

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-require('dotenv').config()
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
 
-
-console.log(PORT)
+console.log(PORT);
+app.listen(PORT, () => {
+  console.log("start");
+});
